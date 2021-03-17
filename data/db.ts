9c36cd2d9a916662124a2867f7d2d5e87bb82a1b
Zack Krida <zackkrida@pm.me>
2021-03-17 11:21:28 -0400

    Data needs for vendor shop

--- data/db.ts
+++ data/db.ts
@@ -1,5 +1,7 @@
 import { Knex, knex } from "knex"
 import { parse } from "pg-connection-string"
+import { map, pluck, compose, evolve, unless, isNil } from "ramda"
+
 const pgconfig = parse(process.env.PG_CONNECTION_STRING as string)
 
 if (process.env.NODE_ENV === "production") {
@@ -23,5 +25,83 @@ export interface Color {
   stack: string
 }
 
+export type CompanyColor =
+  | {
+      id: number
+      name: string
+      company_id: number
+
+      hlc_number: string
+      cn_number: string
+
+      product_hex: string
+      product_number: string
+
+      order_id_created?: number
+
+      /** @deprecated palette metadata */
+      no_palette: boolean
+      /** @deprecated palette metadata */
+      season?: string
+      /** @deprecated palette metadata */
+      year?: string
+      /** @deprecated palette metadata */
+      brand?: string
+      /** @deprecated palette metadata */
+      division?: string
+    }
+  | Timestamps
+
+/**
+ * A unified type between company colors and colors for presentation in the UI.
+ */
+export type UIColor = {
+  number: string
+  name: string
+  hex: string
+}
+
+interface Timestamps {
+  created_at: Date
+  deleted_at?: Date
+  updated_at: Date
+}
+
+export type Company = {
+  id: number
+  name: string
+  quickbooks_id: number
+  chromashare_palette: string
+  discount_percentage: number
+  allow_invoice: boolean
+}
+
+const dateToISO = (date: Date) => date.toISOString()
+const safeDateToISO = unless(isNil, dateToISO)
+
+const timestampsToString = evolve({
+  created_at: safeDateToISO,
+  updated_at: safeDateToISO,
+  deleted_at: safeDateToISO,
+})
+
 export const users = db("users")
 export const colors = db<Color>("colors")
+export const companyColors = db<CompanyColor>("company_colors")
+export const company = db<Company>("companies")
+
+export const colorsFromCompanies = (retailerIDs: string[]) =>
+  companyColors.where("deleted_at", null).whereIn(
+    "company_id",
+    // @ts-ignore
+    compose(map(Number), pluck("retailer_id"))(retailerIDs)
+  )
+
+// vendor company id
+export const vendorRetailerColors = (vendorId: number) =>
+  db("company_retailers_pivot")
+    .where("company_id", vendorId)
+    .select(["retailer_id"])
+    .then(colorsFromCompanies)
+    // @ts-ignore
+    .then(map(timestampsToString))
---
+++ pages/shop/vendor.tsx
@@ -0,0 +1,56 @@
+/**
+ * Vendor Store
+ *
+ * A special version of the store for *vendor* users to purchase company colors
+ * from their retailers. Shows colors grouped by retailer.
+ */
+import { DD } from "components/DD"
+import { Company, CompanyColor, vendorRetailerColors, company } from "data/db"
+import { GetStaticProps } from "next"
+import { uniq, compose, pluck, filter, propEq } from "ramda"
+
+// @ts-ignore
+const retailerIds = compose(uniq, pluck("company_id"))
+// @ts-ignore
+const retailerColors = (retailerID: string | number) =>
+  filter(propEq("company_id", retailerID))
+
+export default function VendorStore({
+  colors,
+  retailers,
+}: {
+  colors: CompanyColor[]
+  retailers: Company[]
+}) {
+  return (
+    <div>
+      <DD label="colors" json={colors} />
+      <DD label="retailers" json={retailers} />
+
+      {retailers.map((i) => (
+        <div key={i.id}>
+          <h2>{i.name}</h2>
+          <ul>
+            {retailerColors(i.id)(colors).map((i) => (
+              <div>{i.name}</div>
+            ))}
+          </ul>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export const getStaticProps: GetStaticProps = async (context) => {
+  const colors = await vendorRetailerColors(4579)
+  // @ts-ignore
+  const retailers = await company.whereIn("id", retailerIds(colors))
+
+  return {
+    revalidate: 1,
+    props: {
+      colors,
+      retailers,
+    },
+  }
+}
